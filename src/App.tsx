9334cd6f81f1f3b01db4a0f5dabcd2ceb98d9907
Lucasccgomes <lucasccgomes@protonmail.com>
2024-01-23 21:43:42 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Política de Privacidade para Previsão do Tempo RAPIDA</h1>
+
+      <></>Esta Política de Privacidade descreve nossas políticas e procedimentos sobre a coleta, uso e divulgação de suas informações ao usar o aplicativo Previsão do Tempo RAPIDA e informa sobre seus direitos de privacidade e como a lei o protege.
+
+      Coleta e Uso de Informações Pessoais
+
+      Enquanto usa nosso Aplicativo, podemos pedir que você nos forneça certas informações pessoalmente identificáveis que podem ser usadas para contatá-lo ou identificá-lo. As informações pessoalmente identificáveis podem incluir, mas não estão limitadas a:
+
+      Endereço de Email
+      Dados de uso
+      Dados de Uso
+
+      O aplicativo pode coletar dados sobre como o serviço é acessado e usado. Estes dados de uso podem incluir informações como o endereço de protocolo de Internet do seu dispositivo (por exemplo, endereço IP), tipo de navegador, versão do navegador, as páginas do nosso serviço que você visita, a hora e data da sua visita, o tempo gasto nessas páginas, identificadores únicos de dispositivos e outros dados de diagnóstico.
+
+      Uso de Dados
+
+      Previsão do Tempo RAPIDA usa os dados coletados para diversos fins:
+
+      Para fornecer e manter nosso serviço
+      Para notificá-lo sobre mudanças em nosso serviço
+      Para permitir que você participe de recursos interativos de nosso serviço quando escolher fazê-lo
+      Para fornecer suporte ao cliente
+      Para coletar análises ou informações valiosas para que possamos melhorar nosso serviço
+      Para monitorar o uso do nosso serviço
+      Para detectar, prevenir e tratar de questões técnicas
+      Transferência de Dados
+
+      Suas informações, incluindo Dados Pessoais, podem ser transferidas para – e mantidas em – computadores localizados fora do seu estado, província, país ou outra jurisdição governamental onde as leis de proteção de dados podem diferir das da sua jurisdição.
+
+      Divulgação de Dados
+
+      Previsão do Tempo RAPIDA pode divulgar seus Dados Pessoais acreditando de boa fé que tal ação é necessária para:
+
+      Cumprir uma obrigação legal
+      Proteger e defender os direitos ou propriedade de Previsão do Tempo RAPIDA
+      Prevenir ou investigar possíveis irregularidades em conexão com o serviço
+      Proteger a segurança pessoal dos usuários do serviço ou do público
+      Proteger contra responsabilidade legal
+      Segurança dos Dados
+
+      A segurança dos seus dados é importante para nós, mas lembre-se de que nenhum método de transmissão pela internet ou método de armazenamento eletrônico é 100% seguro. Embora nos esforcemos para usar meios comercialmente aceitáveis para proteger seus Dados Pessoais, não podemos garantir sua segurança absoluta.
+
+      Alterações nesta Política de Privacidade
+
+      Podemos atualizar nossa Política de Privacidade de tempos em tempos. Notificaremos você sobre quaisquer alterações publicando a nova Política de Privacidade nesta página.
+
+      Você é aconselhado a revisar esta Política de Privacidade periodicamente para quaisquer alterações. Alterações nesta Política de Privacidade são efetivas quando são postadas nesta página.
+
+      Contato
+
+      Se você tiver alguma dúvida sobre esta Política de Privacidade, entre em contato conosco.
+
+
+    </>
+  )
+}
+
+export default App
